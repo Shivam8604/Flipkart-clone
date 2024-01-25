2623df2b4861622df69d80d33cddc24ccebb375a
Shivam8604 <145273089+Shivam8604@users.noreply.github.com>
2024-01-25 13:23:35 +0530

    complete signup page

---
+++ signup.js
@@ -0,0 +1,34 @@
+let getUserData = JSON.parse(localStorage.getItem("userData")) || [];
+
+// console.log(getUserData)
+
+
+function Signup(){
+    let username = document.getElementById("naam").value;
+    let mobile = document.getElementById("phone").value;
+    let password = document.getElementById("pass").value;
+
+    if(mobile.length ==0){
+        alert("Enter your mobile number")
+    }
+    else if(mobile.length !== 10){
+        alert("mobile number should have 10 digits")
+    }
+    else{
+        let userData = {
+            username:username,
+            mobile:mobile,
+            password:password,
+        }
+
+        let users = [...getUserData,userData];
+        users.push(userData);
+        localStorage.setItem("userData", JSON.stringify(users));
+
+        alert("Signup Successful");
+        window.location.href = "sign.html";
+    }
+
+}
+
+// console.log(Signup())
